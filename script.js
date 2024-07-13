8a549b8b85a75f24c77540d8f05ca3166f8c7ff1
Utsab Adhikari <whitewebnepal@gmail.com>
2024-07-13 16:31:48 +0545

    save changes

---
+++ script.js
@@ -0,0 +1,31 @@
+function openNav() {
+    document.getElementById("navmob").style.width = "250px";
+    document.querySelector('.content').classList.add('blur');
+}
+
+function closeNav() {
+    document.getElementById("navmob").style.width = "0";
+    document.querySelector('.content').classList.remove('blur');
+}
+
+function openProfile() {
+    document.getElementById("profilePopup").style.display = "block";
+    document.querySelector('.content').classList.add('blur');
+}
+
+function closeProfile() {
+    document.getElementById("profilePopup").style.display = "none";
+    document.querySelector('.content').classList.remove('blur');
+}
+
+function openDevelopers() {
+    document.getElementById("developersPopup").style.display = "block";
+    document.querySelector('.content').classList.add('blur');
+}
+
+function closeDevelopers() {
+    document.getElementById("developersPopup").style.display = "none";
+    document.querySelector('.content').classList.remove('blur');
+}
+  // JavaScript to dynamically set the current year
+  document.getElementById("currentYear").textContent = new Date().getFullYear();
